659369facf9d2907dbab7ad24da50dd67adaa0a3
r0ckwe11 <oleg.kashu@mail.ru>
2024-10-26 02:59:20 +0300

    tasks and report

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,32 @@
 package main
 
+import "fmt"
+
 // реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	output := make(chan int)
+	go func() {
+		defer close(output)
+		select {
+		case a := <-firstChan:
+			output <- a * a
+		case b := <-secondChan:
+			output <- b * 3
+		case <-stopChan:
+			return
+		}
+
+	}()
+	return output
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	ch1, ch2 := make(chan int), make(chan int)
+	stop := make(chan struct{})
+
+	r := calculator(ch1, ch2, stop)
+	ch1 <- 4
+	// ch2 <- 3
+	close(stop)
+	fmt.Println(<-r)
 }
--- projects/work/main.go
+++ projects/work/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
-	// "sync"
 )
 
 func work() {
@@ -12,5 +12,14 @@ func work() {
 }
 
 func main() {
-	// необходимо в отдельных горутинах вызвать функцию work() 10 раз и дождаться результатов выполнения вызванных функций
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			work()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
